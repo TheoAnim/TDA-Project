39b7bde149e52043f849ee8cfd8425d32eca1e25
Theo Anim <theophilus.animbediako@jacks.sdstate.edu>
2025-08-12 15:30:45 -0500

    KNN added

--- Mnist ML methods.R
+++ Mnist ML methods.R
@@ -27,12 +27,37 @@ ggplot(train_df_long)+
 #take the train images from the slides .qmd file
 #normalized pixel values and apply t-SNE
 library("Rtsne")
-tsne_results <- Rtsne(train_images, dim = 2, perplexity = 30, max_iter = 1000)
-df_tsne <- tibble(Dim1 = tsne_results$Y[, 1], Dim2 = tsne_results$Y[, 2], digit = train_df$labels)
-ggplot(df_tsne, aes(Dim1, Dim2, color = digit))+
+# tsne_results <- Rtsne(
+#   train_images,
+#   dim = 2,
+#   perplexity = 30,
+#   max_iter = 1000
+# )
+# df_tsne <- tibble(Dim1 = tsne_results$Y[, 1],
+#                   Dim2 = tsne_results$Y[, 2],
+#                   digit = train_df$labels)
+#
+# saveRDS(df_tsne, "df_tsne")
+df_tsne <- readRDS("df_tsne")
+ggplot(df_tsne, aes(Dim1, Dim2, color = digit)) +
   geom_point(alpha = .8)
 
 #----------------------------------------------------------
 #----------------------------KNN---------------------------
+library(caret)
+knn_trainControl <- trainControl(method = "cv",
+                               number = 10)
+# train_knn <- train(
+#   labels ~ .,
+#   train_df,
+#   method = "knn",
+#   metric = "Accuracy",
+#   trControl = knn_trainControl
+# )
+# saveRDS(train_knn, "train_knn")
 
+knn_train <- readRDS("train_knn")
+plot(train_knn)
 
+knn_prediction <- predict(train_knn, newdata = test_images)
+print(knn_prediction)
